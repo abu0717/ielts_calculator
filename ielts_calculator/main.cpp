a25cd7acb8d1232a73ee6834e1418641dea916dd
abu0717 <nosirovabdulloh952@gmail.com>
2024-10-29 21:36:15 +0500

    Initial Commit

---
+++ ielts_calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ielts_calculator
+//
+//  Created by MacBook Air on 29/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
